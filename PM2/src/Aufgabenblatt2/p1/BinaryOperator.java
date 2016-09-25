1de33a3cd13d9d04378fe50ae1786dc213e140f2
Stefan Belic <stefan_belic@hotmail.de>
2016-09-25 15:13:18 +0200

    Aufgabenblatt 2 test und ein bissen .1 gemacht

---
+++ PM2/src/Aufgabenblatt2/p1/BinaryOperator.java
@@ -0,0 +1,8 @@
+package Aufgabenblatt2.p1;
+
+@FunctionalInterface
+public interface BinaryOperator {
+	
+	public double operation(double x,double y);
+
+}
--- PM2/src/Aufgabenblatt2/p1/Rechner.java
+++ PM2/src/Aufgabenblatt2/p1/Rechner.java
@@ -1,5 +1,31 @@
 package Aufgabenblatt2.p1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Rechner {
 	
+	private Map<Operation,BinaryOperator> rechnerMap;
+	
+	public Rechner(){
+		rechnerMap = new HashMap<Operation,BinaryOperator>();
+		rechnerMap.put(Operation.ADDITION, (x,y) -> {
+			return x+y;
+		});
+		rechnerMap.put(Operation.SUBTRAKTION, (x,y) -> {
+			return x-y;
+		});
+		rechnerMap.put(Operation.MULTIPLIKATION, (x,y) -> {
+			return x*y;
+		});
+		rechnerMap.put(Operation.DIVISION, (x,y) -> {
+			return x/y;
+		});
+	}
+	public double berechne(Operation operation,double x,double y){
+		
+		BinaryOperator binaryOperation = rechnerMap.get(operation);
+		return binaryOperation.operation(x, y);
+		
+	}
 }
